9681c29408914f667ae405589fc0b2af3d5f6a1c
XuDarkholme <122281083+XuDarkholme@users.noreply.github.com>
2023-03-20 18:19:46 +0800

    Update Tree_order.c

--- C_practice/Binary_tree/Tree_order.c
+++ C_practice/Binary_tree/Tree_order.c
@@ -1,13 +1,3 @@
-#include <stdio.h>
-#include <stdlib.h>
-
-typedef struct node
-{
-	int data;
-	struct node* left;
-	struct node* right;
-}Node;
-
 void preorder(Node*node)	//前序遍历
 {
 	if (node != NULL)
@@ -36,33 +26,4 @@ void postorder(Node* node)	//后序遍历
 		postorder(node->right);
 		printf("%d\t", node->data);
 	}
-}
-
-int main()
-{
-	Node n1;
-	Node n2;
-	Node n3;
-	Node n4;
-
-	n1.data = 5;
-	n2.data = 6;
-	n3.data = 7;
-	n4.data = 8;
-
-	n1.left  = &n2;
-	n1.right = &n3;
-	n2.left  = &n4;
-	n2.right = NULL;
-	n3.left  = NULL;
-	n3.right = NULL;
-	n4.left  = NULL;
-	n4.right = NULL;
-
-	printf("前序遍历：\n");
-	preorder(&n1);
-	printf("\n中序遍历：\n");
-	inorder(&n1);
-	printf("\n后序遍历：\n");
-	postorder(&n1);
 }
